31adef63cb3fb2c91fbdf9adcfb401619ba0e288
BaristaBarita <18020776@usc.edu.ph>
2021-11-30 20:43:30 +0800

    Arigathankies for helping :,>

---
+++ javascripts/script.js
@@ -0,0 +1,105 @@
+// sidebar open close JS code
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let closeOpenBtn = document.querySelector(".nav-links .bx-x");
+let navLinks = document.querySelector(".nav-links");
+
+menuOpenBtn.addEventListener("click", ()=>{
+    navLinks.style.left="0";
+});
+closeOpenBtn.addEventListener("click", ()=>{
+    navLinks.style.left="-100%";
+});
+
+// sidebar sub menu open close JS code
+
+let gameinfoArrow = document.querySelector(".gameinfo-arrow");
+gameinfoArrow.addEventListener("click", ()=>{
+   navLinks.classList.toggle("show1"); 
+});
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.addEventListener("click", ()=>{
+   navLinks.classList.toggle("show2"); 
+});
+
+
+
+//Carousel Picture switching JS code
+    const slider = document.querySelector(".slider");
+    const nextBtn = document.querySelector(".next-btn");
+    const prevBtn = document.querySelector(".prev-btn");
+    const slides = document.querySelectorAll(".slide");
+    const slideIcons = document.querySelectorAll(".slide-icon");
+    const numberOfSlides = slides.length;
+    var slideNumber = 0;
+
+     //image slider next button
+    nextBtn.addEventListener("click", () => {
+      slides.forEach((slide) => {
+        slide.classList.remove("active");
+      });
+      slideIcons.forEach((slideIcon) => {
+        slideIcon.classList.remove("active");
+      });
+
+      slideNumber++;
+
+      if(slideNumber > (numberOfSlides - 1)){
+        slideNumber = 0;
+      }
+
+      slides[slideNumber].classList.add("active");
+      slideIcons[slideNumber].classList.add("active");
+    });
+
+    //image slider previous button
+    prevBtn.addEventListener("click", () => {
+      slides.forEach((slide) => {
+        slide.classList.remove("active");
+      });
+      slideIcons.forEach((slideIcon) => {
+        slideIcon.classList.remove("active");
+      });
+
+      slideNumber--;
+
+      if(slideNumber < 0){
+        slideNumber = numberOfSlides - 1;
+      }
+
+      slides[slideNumber].classList.add("active");
+      slideIcons[slideNumber].classList.add("active");
+    });
+
+    //image slider autoplay
+    var playSlider;
+
+    var repeater = () => {
+      playSlider = setInterval(function(){
+        slides.forEach((slide) => {
+          slide.classList.remove("active");
+        });
+        slideIcons.forEach((slideIcon) => {
+          slideIcon.classList.remove("active");
+        });
+
+        slideNumber++;
+
+        if(slideNumber > (numberOfSlides - 1)){
+          slideNumber = 0;
+        }
+
+        slides[slideNumber].classList.add("active");
+        slideIcons[slideNumber].classList.add("active");
+      }, 4000);
+    }
+    repeater();
+
+    //stop the image slider autoplay on mouseover
+    slider.addEventListener("mouseover", () => {
+      clearInterval(playSlider);
+    });
+
+    //start the image slider autoplay again on mouseout
+    slider.addEventListener("mouseout", () => {
+      repeater();
+    });
